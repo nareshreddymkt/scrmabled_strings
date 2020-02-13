851fc69fec30616ca78868215f49a5cb38ee963d
nareshreddymkt <35947161+nareshreddymkt@users.noreply.github.com>
2020-02-13 10:34:28 +0530

    Update scrmabled_strings.sh

--- scrmabled_strings.sh
+++ scrmabled_strings.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-# Scrambled Word Count -Python (Docker:python:3.7-alpine)
-#               author: Naresh Reddy M 
-
-# Get the docker image for Scrambled application.
-docker pull nareshreddymkt/python-scrambled-wc
-
-# Run a container for the above docker image.
-
-docker run -v "$1":/code/files/dictionary_words -v "$2":/code/files/search_string  -it nareshreddymkt/python-scrambled-wc
+python ./code/scrmabled_strings.py --dictionary ./code/files/dictionary_words --input ./code/files/search_string
